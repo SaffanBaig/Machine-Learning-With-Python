139f38dd0936dcdc6bd08ab3afdaca330b880310
Saffan baig <mirzasaffan492@gmail.com>
2019-09-24 11:22:38 -0700

    Simple Linear Regression

--- datapreprocessing/Data-preprocessing-template.py
+++ datapreprocessing/Data-preprocessing-template.py
@@ -16,10 +16,13 @@ Y = dataset.iloc[:, 3].values
 # y contains all the dependent variables
 
 #missing data
-from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder
+#from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder, StandardScaler
+#imputer = SimpleImputer(missing_values="NaN", strategy="mean")
 imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+#X[:, 1:3] = imputer.fit_transform(X[:, 1:3])
 
 #Categorical Data
 labelencoder_X = LabelEncoder()
@@ -28,4 +31,13 @@ onehotencoder = OneHotEncoder(categorical_features=[0])
 X = onehotencoder.fit_transform(X).toarray()
 
 labelencoder_Y = LabelEncoder()
-Y[:, 0] = labelencoder_Y.fit_transform(Y[:, 0])
+Y = labelencoder_Y.fit_transform(Y)
+
+# Making training and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+#feature scaling
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
